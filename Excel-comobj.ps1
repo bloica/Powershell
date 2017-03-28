bd659f1d999d7c502efd8f22fd926905de38c010
bloica <bloica@gmail.com>
2017-03-28 12:26:30 +0200

    Add files via upload

---
+++ Excel-comobj.ps1
@@ -0,0 +1,21 @@
+﻿$excel = New-Object -ComObject Excel.Application
+
+$excel.Visible = $true
+
+$workbook = $excel.Workbooks.Add()
+
+$sheet = $workbook.ActiveSheet
+
+$counter = 0
+
+Get-process | ForEach-Object {
+
+    $counter++
+
+    $sheet.cells.Item($counter,1) = $_.Name
+
+    $sheet.cells.Item($counter,2) = $_.DisplayName
+
+    $sheet.cells.Item($counter,3) = $_.Status
+
+}
